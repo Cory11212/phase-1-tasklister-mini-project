a4c9f9d1db8fc831be58fd018e9b93e471a39ee8
Cory <corymclean0@gmail.com>
2023-09-24 23:38:08 -0400

    boy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    addTodo(e.target.querySelector('#new-task-description').value)
+  })
 });
+
+function addTodo(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteTodo)
+  btn.textContent = 'x'
+  li.innerText = `${todo} `
+  li.append(btn)
+  document.querySelector('#tasks').append(li)
+}
+
+function deleteTodo(e){
+  e.target.parentNode.remove()
+}
